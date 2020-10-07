7ac110b485d98291e4e85a936684db3984f816b8
chin <greeting.to.chin@gmail.com>
2020-10-07 22:07:23 +0800

    Init project

---
+++ README.md
@@ -0,0 +1,6 @@
+## The world needs the rule.
+
+Quick and easy for project setup.
+Nice and clean code is this project's purpose.
+
+😇😇😇
---
+++ dist/share.js
@@ -0,0 +1,26 @@
+module.export = {
+  extends: [
+    'eslint:recommended',    
+    'prettier',
+    'prettier/react',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended'
+  ],
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'prettier'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'none',
+        printWidth: 100,
+      },
+    ],
+    'import/extensions': [
+      'off',
+      'ignorePackages'
+    ]
+  }
+}
